4a3f67f3b66c262fcb34f7ce7b35a0f0712961c2
drkarthi <karthikthegoth@gmail.com>
2016-02-01 17:18:36 -0500

    algo to invert binary tree

---
+++ invert_binary_tree.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // base case
+        if(root == NULL)
+        {
+            return NULL;
+        }
+        // swap the right and left values 
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
